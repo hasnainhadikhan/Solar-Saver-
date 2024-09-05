ea8191f1ee0428fd03458ca5182bf69d51ac6242
Hasnainhadi2006 <153942494+hasnainhadikhan@users.noreply.github.com>
2024-09-05 13:17:04 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,272 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+ <div className="container">
+   <img className='navbar-logo' src='https://solarsaver.vercel.app/assets/logo-CeUqb7XY.png'/>
+   <div className="navbar-main" id="navbarNav">
+      <ul className="nav-Item">
+        <li>
+          <a className="nav-Link" href="#">Pricing</a>
+        </li>
+        <li>
+          <a className="nav-Link" href="#">About</a>
+        </li>
+        <li>
+          <a className="nav-Link" href="/">Tool</a>
+        </li>
+        <li>
+          <a className="nav-Link">How its Work</a>
+        </li>
+        <li>
+          <a className="nav-Link" href="/">Testinomail</a>
+        </li>
+        <li>
+          <a className="nav-Link">FQA</a>
+        </li>
+  </ul>
+  <button className="nav-Btn">Calculate Solar</button> 
+</div>
+</div>
+  <div id='sectionTwo'>
+  <div className='headingMain'>
+       <h1>
+     <span>Does Your Home</span> Need Quality<br/>
+       Solar Panel Installation?
+       </h1>
+      <h4 id='para3'>Trust our experienced solar power experts to customize the perfect solution for your home.</h4>
+    
+       <button className="nav-Btn2">Calculate Solar</button> 
+     <img src='https://solarsaver.vercel.app/assets/cover-C4PYW05n.webp' className='solar-image' alt='Main Image'/>
+  </div>
+  </div>
+<div className='sectionthree'>
+ <h2>TRANSPARENT PRICING</h2>
+ <div className='card-Main'>
+ <div className="card">
+  <div className="card-body">
+     <div id='Image'>
+    <img src='https://cdn.icon-icons.com/icons2/520/PNG/512/House_icon-icons.com_52056.png' className='card-img'/>
+     </div>
+    <h2 className="card-title"><span>STANDARD</span> PLAN</h2>
+    <h6 className="card-text">
+              Advanced solar technology designed for <br/>
+              maximum energy savings. Ideal for <br/>
+              larger Homes and Estates..</h6>
+              <h4>$2.00 / watt</h4>
+    <button className="btn3">Calculate Solar</button>
+ </div>
+</div>
+<div className="card  ">
+  <div className="card-body">
+    <div id='Image'>
+    <img src='https://cdn.icon-icons.com/icons2/520/PNG/512/House_icon-icons.com_52056.png' className='card-img'/>
+     </div>
+    <h2 className="card-title">   
+   <span>PERMIEM</span> PLAN  
+    </h2>
+    <h6 className="card-text">
+      Efficient and affordable solar solutions for<br/>
+       every Household. Perfect for Residential<br/> 
+       use.</h6>
+       <h4>$1.75 / watt</h4>
+       <button className="btn3">Calculate Solar</button>
+ </div>
+</div>
+<div className="card  ">
+  <div className="card-body">
+     <div id='Image'>
+    <img src='https://cdn.icon-icons.com/icons2/520/PNG/512/House_icon-icons.com_52056.png' className='card-img'/>
+    </div>
+    <h2 className="card-title"><span>COMMERCIAL</span> PLAN</h2>
+    <h6 className="card-text">
+      High-capacity solar systems tailored for <br/>
+      Businesses and Enterprises. Reliable and <br/>
+      Robust solutions..</h6>
+      <h4>$1.50 / watt</h4>
+      <button className="btn3">Calculate Solar</button>
+ </div>
+</div>
+</div>
+</div>
+<div className='sectionfour'>
+ <h5 className='about-h5'>About Us</h5>
+ <h1 className='about-h1'>SOLAR SAVER</h1>
+  <p className='about-Text'>
+  Solar Saver is revolutionizing the solar industry by offering <br/>
+  Most cost-effective solar solutions in America. We specialize in<br/> 
+  Providing transparent, instant pricing for solar installations. With <br/>
+  An innovative AI quoting engine, they ensure personalized and <br/>
+  Accurate estimates for each customer.
+  </p>
+  <p className='about-Text2'>
+  Solar Saver also offers innovative battery solutions and flexible<br/>
+  financing options, making solar energy more accessible and <br/>
+  affordable.
+  </p>
+  <div id='the-image'>
+  <img src='https://solarsaver.vercel.app/assets/about-img-CpwndtbG.webp' className='img-right'
+/>
+</div>
+</div>
+<div id='sectionfive'>
+<hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr/><hr></hr><hr/><hr/><hr/>
+<div className='boxes'> 
+<div className=" box"  >
+  <h4>System Size</h4><
+  p>9.5 - 14.13 kWh*</p>
+  </div>
+<div className="box" id='box2'>
+  <h4>Solar Panels</h4>
+<p>34 - 43*</p>
+</div>
+<div className="box" id='box3'>
+  <h4>CO2 Offset</h4>
+<p>7.65 - 9.07*</p>
+</div>
+</div>
+
+<div className='section-right'>
+<h1><span>AI</span> Utilization</h1>
+</div>
+<p  >
+Solar Saver utilizes an AI quoting engine to estimate the <br/>
+ideal solar system size for a customer's specific needs. This <br/>
+system calculates the number of solar panels required, the<br/> 
+potential CO2 offset, and the new, likely reduced<br/>
+monthly power bill.
+<br/><br/>
+By analyzing various factors such as roof size, local sun <br/>
+exposure, and current electricity usage, the AI provides <br/>
+atailored solution.
+<br/><br/>
+<span>This approach ensures customers get a system <br/>
+   that maximizes energy efficiency and cost savings<br/>
+   while contributing to environmental sustainability.<br/>
+   </span>
+</p>
+</div>
+ <div className='section-six'> 
+ <h1><span>BEFORE</span>  & After</h1>
+<h5 id='p2'>There are countless success stories of Solar Saver transformations, but the<br/>
+    majority showcase significant improvements in energy efficiency and cost savings.</h5>
+<img  src='https://solarsaver.vercel.app/assets/after-slider-zGc8hDen.webp' className='Sec-Img'/>
+</div>
+<div className='section-seven'>
+  <h1><span>Best </span> Equipment in the industry</h1>
+  <h5  id='p2' > Solar Saver uses the highest quality solar panels and inverters. <br/>
+             Trust our cutting-edge technology for superior performance and durability.</h5>
+    <div className='card-Main' >
+      <div className="card" id='cardchange'>
+  <div className="card-body">
+    <div id='img7'>
+    <img src='https://cdn.icon-icons.com/icons2/520/PNG/512/House_icon-icons.com_52056.png' className='homeimg'/>
+     </div>
+    <h2 className="title">PANELS INFO</h2>
+    <p className="card-text">
+    Solar Saver supplies high-perfor<br/>
+    mance inverters.Our inverters  <br/>
+    ensure maximum energy conversion <br/> 
+     and system reliability.
+  </p>
+       
+ </div>
+</div>
+<div className="card " id='cardchange'>
+  <div className="card-body">
+     <div id='img7'>
+    <img src='https://cdn.icon-icons.com/icons2/520/PNG/512/House_icon-icons.com_52056.png' className='homeimg'/>
+    </div>
+    <h2 className="title"> INVERTERS</h2>
+    <p className="card-text">
+    Solar Saver offers industry-leading <br/>
+    solar panels. Experience unpara<br/>
+    lleled efficiency and durability<br/>
+     with our advanced technology.<br/>
+       
+     </p>
+
+ </div></div>
+ <div className="card" id='cardchange'>
+  <div className="card-body">
+     <div id='img7'>
+    <img src='https://cdn.icon-icons.com/icons2/520/PNG/512/House_icon-icons.com_52056.png' className='homeimg'/>
+    </div>
+    <h2 className="title">BATTERIES </h2>
+    <p className="card-text">
+    Solar Saver provides state-of-the-<br/>
+    art batteries. Enjoy consistent and <br/>
+    long lasting energy storage with our <br/>
+    premium solutions.
+  .</p>
+
+ </div></div>
+</div>
+</div>
+   
+<div className='sectioneight'>
+<h1><span>WE'VE</span>GOT YOU COVERED</h1>
+<h5 id='p2'>Explore solar potential in your area. See how Solar Saver can help you.</h5>
+<img src='https://solarsaver.vercel.app/assets/map-img1-DR_uFjwz.webp' className='map'/>
+</div>
+<div className='scetionnine'>
+ <img src='https://solarsaver.vercel.app/assets/joh-doe-CpqoclS1.webp' className='man'/>
+ <h1 className='hh'>
+  <span className='spam'>"</span>
+  <br/>
+ Solar Saver exceeded my expectations in <br/>
+ every way. From the initial consultation<br/>
+ to the final installation, their team was<br/>
+  professional and efficient. The whole <br/>
+ process was seamless. <br/> 
+ Highly recommend!
+ </h1>
+<br/><br/> <h6><img src='https://solarsaver.vercel.app/assets/joh-doe-CpqoclS1.webp' className='man2'/>        @ Joh Doe</h6>
+</div>
+<div className='footer'> 
+
+      <img src='https://solarsaver.vercel.app/assets/before-slider-5RDoAMwp.webp' alt='footer-img' className='img-footer'/>
+      <h1 className='footer-h1'>Ready To Start</h1>
+      <p id='footer-p'>Our solar power experts will help you find the perfect solution for your home.</p>
+      <button className='btn4'>Calculate Solar</button>
+     <div>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+     <div className="container">
+   <img className='navbar-logo2' src='https://solarsaver.vercel.app/assets/logo-CeUqb7XY.png'/>
+   <div className="navbar-main" id="navbarNav">
+      <ul className="nav-Item">
+        <li>
+          <a className="nav-Link" href="#">Pricing</a>
+        </li>
+        <li>
+          <a className="nav-Link" href="#">About</a>
+        </li>
+        <li>
+          <a className="nav-Link" href="/">Tool</a>
+        </li>
+        <li>
+          <a className="nav-Link">How its Work</a>
+        </li>
+        <li>
+          <a className="nav-Link" href="/">Testinomail</a>
+        </li>
+        <li>
+          <a className="nav-Link">FQA</a>
+        </li>
+  </ul>
+</div>
+</div>
+</div>
+</div>
+<hr className='hr-footer'/>
+<h3 className='last-tag'> Developed By<span> Hasnainhadi</span></h3>
+</>
+  ) 
+}
+
+export default App
